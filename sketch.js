82fb20ef7a52fec2bc3ba5c099045350fda16462
trhill2 <Trhill2@dons.usfca.edu>
2022-01-27 15:24:05 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+
+  textSize(48);
+  textAlign(CENTER);  
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+
+  fill(80,0,200);
+  text('Hello World', width/2, height/2);
+
+}
